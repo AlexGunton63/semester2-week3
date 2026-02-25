d5c8cbb8e60978655719596ad77c1d05b2d00517
AlexGunton63 <kbzz0907@leeds.ac.uk>
2026-02-25 17:28:47 +0000

    fixed tax calcs

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -16,8 +16,14 @@
     // calculate the deductions and final take-home salary
     float NI_cost = salary * NI; // national insurance deduction
     float NI_salary = salary - NI_cost;
-    float tax_cost = NI_salary *TR; // tax deduction
+
+    //float tax_cost = NI_salary *TR; // tax deduction
+    //float take_home_salary = NI_salary - tax_cost;  // final salary
+
+    float taxable_salary = NI_salary - 12500;
+    float tax_cost = taxable_salary * TR;
     float take_home_salary = NI_salary - tax_cost;  // final salary
+
     // Use only these print statement with appropriate formatting and variable names
     //printf("Salary £...",var_name);
     printf("Salary £%.2f",salary);
